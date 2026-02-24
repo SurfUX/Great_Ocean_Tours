5f40caaa3a8a0cfbae18ba028959798ef6289207
SurfUX <tecnetsoftwaresolution@gmail.com>
2026-02-24 14:15:41 +0530

    Map Changes

--- contact.php
+++ contact.php
@@ -133,7 +133,7 @@
       </div>
       <!-- Map Section -->
       <div class="map-container mb-5">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d48389.78314118045!2d-74.006138!3d40.710059!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c25a22a3bda30d%3A0xb89d1fe6bc499443!2sDowntown%20Conference%20Center!5e0!3m2!1sen!2sus!4v1676961268712!5m2!1sen!2sus" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3690.388452230997!2d114.1990125!3d22.3389568!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x340401c9b690d1d5%3A0xa91cd57aed965295!2sGreat%20Ocean%20Tours%20%26%20Travel%20Ltd.%20Hong%20Kong!5e0!3m2!1sen!2slk!4v1771898254146!5m2!1sen!2slk" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
 
     </section><!-- /Contact Section -->
